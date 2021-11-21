53ce2815681a2b97790032627ba85471b38727f2
Alina45867 <alina08091976@yandex.ru>
2021-11-22 00:11:49 +0300

    Sprint 5. Tests

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -1,36 +1,31 @@
 import shutil
 import tempfile
-from http import HTTPStatus
-from django.shortcuts import redirect
 from posts.forms import PostForm
 from posts.models import Post, Group, User
 from django.conf import settings
-from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import Client, TestCase, override_settings
 from django.urls import reverse
 
 
 TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
 
+
 @override_settings(MEDIA_ROOT=TEMP_MEDIA_ROOT)
 class PostFormTests(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-       
         cls.user = User.objects.create_user(username='user')
         cls.group = Group.objects.create(
-                title = 'Test',
-                slug = 'test_slug',
-                description = 'testing',
-
+                title='Test',
+                slug='test_slug',
+                description='testing',
             )
         cls.post = Post.objects.create(
-                author = cls.user,
-                text = 'Test_post',
-                group = cls.group,
+                author=cls.user,
+                text='Test_post',
+                group=cls.group,
             )
-     
         cls.form = PostForm()
 
     @classmethod
@@ -38,7 +33,6 @@ class PostFormTests(TestCase):
         super().tearDownClass()
         shutil.rmtree(TEMP_MEDIA_ROOT, ignore_errors=True)
         
-
     def setUp(self):
         self.guest_client = Client()
         self.user = User.objects.create_user(username='StasBasov')
@@ -47,22 +41,25 @@ class PostFormTests(TestCase):
 
     def test_create_post(self):
         post_count = Post.objects.count()  
-        
         form_data = {
             'text': 'Testing text',
             'group': self.group.id,
-            
-            
+
         }
         response = self.authorized_client.post(
             reverse('posts:create_post'),
             data=form_data,
             follow=True
         )
-  
         self.assertEqual(response.status_code, 200)
-        self.assertRedirects(response, reverse('posts:profile', kwargs={'username': 'StasBasov'}))
-        self.assertEqual(Post.objects.count(), post_count+1)
+        self.assertRedirects(
+            response, reverse(
+                'posts:profile', kwargs={
+                    'username': 'StasBasov'
+                    }
+                )
+            )
+        self.assertEqual(Post.objects.count(), post_count + 1)
         
         self.assertTrue(
             Group.objects.filter(
@@ -73,28 +70,27 @@ class PostFormTests(TestCase):
         )
 
     def test_post_edit(self):
-         
-        
         form_data = {
             'text': 'New testing text',
             'group': self.group.id,
-            
-            
         }
         response = self.authorized_client.post(
             reverse('posts:post_edit', kwargs={
                         'post_id': self.post.id,
-                        }),
+                        }
+                    ),
             data=form_data,
             follow=True
         )
-  
+
         self.assertEqual(response.status_code, 200)
-        self.assertRedirects(response, reverse('posts:post_detail', kwargs={
-            'post_id': self.post.id,
-            }))
-        
-        
+        self.assertRedirects(
+            response, reverse(
+                'posts:post_detail', kwargs={
+                    'post_id': self.post.id,
+                    }
+                )
+            )
         self.assertTrue(
             Group.objects.filter(
                 description='testing',
--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -13,10 +13,10 @@ class TaskPagesTests(TestCase):
         super().setUpClass()
         cls.user = User.objects.create_user(username='user')
         cls.group = Group.objects.create(
-                title='Test',
-                slug='test_slug',
-                description='testing',
-            )
+            title='Test',
+            slug='test_slug',
+            description='testing',
+        )
 
         cls.post = Post.objects.create(
                 author=cls.user,
@@ -32,14 +32,14 @@ class TaskPagesTests(TestCase):
     def test_pages_uses_correct_template(self):
         templates_pages_names = {
             'posts/index.html': reverse('posts:index'),
-            'posts/group_list.html':(
-                reverse('posts:group_list', kwargs={'slug': 'test_slug'})),
+            'posts/group_list.html': (
+               reverse('posts:group_list', kwargs={'slug': 'test_slug'})),
             'posts/profile.html': (
                reverse('posts:profile', kwargs={'username': 'StasBasov'})),
             'posts/post_detail.html': (
-                reverse('posts:post_detail', args='1')),
-            'posts/create_post.html':(
-                reverse('posts:post_edit', args={'slug': 'test-slug'})),
+               reverse('posts:post_detail', args='1')),
+            'posts/create_post.html': (
+               reverse('posts:post_edit', args={'slug': 'test-slug'})),
             'posts/create_post.html': reverse('posts:create_post'),
         }
         for template, reverse_name in templates_pages_names.items():
@@ -49,11 +49,11 @@ class TaskPagesTests(TestCase):
 
     def test_new_page_show_correct_context(self):
         response = self.authorized_client.get(reverse('posts:create_post'))
-      
+
         form_fields = {
             "text": forms.fields.CharField,
             "group": forms.fields.ChoiceField,
-            
+
         }
         for name, expected in form_fields.items():
             with self.subTest(name=name):
@@ -61,11 +61,13 @@ class TaskPagesTests(TestCase):
                 self.assertIsInstance(field_filled, expected)
 
     def test_edit_page_context(self):
-        response = self.authorized_client.get(reverse('posts:post_edit', args = '1'))
+        response = self.authorized_client.get(reverse(
+            'posts:post_edit', args='1'
+            ))
         form_fields = {
             "text": forms.fields.CharField,
             "group": forms.fields.ChoiceField,
-            
+
         }
         for name, expected in form_fields.items():
             with self.subTest(name=name):
@@ -76,21 +78,21 @@ class TaskPagesTests(TestCase):
         response = self.authorized_client.get(reverse('posts:index'))
         post = response.context["page_obj"][0]
         self.assertEqual(post, self.post)
-      
+
     def test_group_page_show_correct_context(self):
         response = self.authorized_client.get(
             reverse('posts:group_list', args=[self.group.slug])
         )
         self.assertEqual(response.context["group"], self.group)
         self.assertIn("page_obj", response.context)
-        
+
     def test_profile_page_show_correct_context(self):
         response = self.authorized_client.get(
             reverse('posts:profile', args=[self.user.username])
         )
         self.assertEqual(response.context['user'], self.user)
         self.assertIn('page_obj', response.context)
-    
+
     def test_post_edit_page_show_correct_context(self):
         response = self.authorized_client.get(
             reverse('posts:post_edit', args='1')
@@ -98,9 +100,10 @@ class TaskPagesTests(TestCase):
         self.assertIn("form", response.context)
         self.assertEqual(response.context["post"], self.post)
 
+
 class PaginatorViewsTest(TestCase):
     @classmethod
-    def setUpClass(cls): 
+    def setUpClass(cls):
         super().setUpClass()
         cls.user = User.objects.create(
             username='posts_author',
@@ -119,38 +122,47 @@ class PaginatorViewsTest(TestCase):
             )
             for i in range(13)]
 
-        def setUp(self):
+        def setUp(self): 
             self.user = User.objects.create_user(username='DanBasov')
             self.authorized_client = Client()
             self.authorized_client.force_login(self.user)
 
-        def first_page_paginator_index(self):
+        def first_page_paginator_index(self): 
             response = self.client.get(reverse('posts:index'))
             self.assertEqual(len(response.context['object_list']), 10)
 
-        def second_page_index_paginator(self):
+        def second_page_index_paginator(self): 
             response = self.client.get(reverse('posts:index') + '?page=2')
-            self.assertEqual(len(response.context['object_list']), 3) 
+            self.assertEqual(len(response.context['object_list']), 3)
 
-        def test_paginator_on_pages(self):
-            first_page_len_posts = 10
+        def test_paginator_on_pages(self): 
+            first_page_len_posts = 10 
             second_page_len_posts = 3
             context = {
                 reverse('posts:index'): first_page_len_posts,
                 reverse('posts:index') + '?page=2': second_page_len_posts,
-                reverse('posts:group_list', kwargs={'slug': self.group.slug, }):
+                reverse('posts:group_list', kwargs={
+                    'slug': self.group.slug, 
+                    }):
                 first_page_len_posts,
-                reverse('posts:group_list', kwargs={'slug': self.group.slug, })
+                reverse('posts:group_list', kwargs={
+                    'slug': self.group.slug, 
+                    })
                 + '?page=2': second_page_len_posts,
-                reverse('posts:profile', kwargs={'username': self.user.username}):
+                reverse('posts:profile', kwargs={
+                    'username': self.user.username
+                    }):
                 first_page_len_posts,
-                reverse('posts:profile', kwargs={'username': self.user.username})
+                reverse('posts:profile', kwargs={
+                    'username': self.user.username
+                    })
                 + '?page=2': second_page_len_posts,
             }
             for reverse_page, len_posts in context.items():
                 with self.subTest(reverse=reverse):
                     self.assertEqual(len(self.client.get(
                         reverse_page).context.get('page')), len_posts)
+
  
 
     
